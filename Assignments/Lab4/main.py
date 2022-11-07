c94409787bf96da6329c7db9943bda80ab19f920
Rajkumar Conjeevaram Mohan <rajkumarcm@yahoo.com>
2022-11-07 18:58:57 -0500

    Start, and end values in even order is wrong. Must take into account two level of MA's

--- Assignments/Lab4/main.py
+++ Assignments/Lab4/main.py
@@ -25,9 +25,10 @@ def ma(y, m=None, sm=None, sf=False):
         # Odd number
         y_hat = np.zeros([len(y), 1])
         k = (m+1)//2 # actual order
-        for t in range(k-1, len(y)):
+        rem = m - k
+        for t in range(k-1, len(y)-rem):
             y_hat[t] = np.mean(y[t-(k-1):t+k])
-        return y_hat[k-1:]
+        return y_hat[k-1:(len(y)-rem)], k-1, len(y)-rem
 
     else:
         # Even number
@@ -35,20 +36,20 @@ def ma(y, m=None, sm=None, sf=False):
             y_hat = np.zeros([len(y), 1])
         else:
             y_hat = np.zeros([len(y), 2])
-        k = m//2
-
+        k = m//2 - 1
+        rem = m - (k+1)
         if m == 2:
-            for t in range(k, len(y)):
-                y_hat[t, 0] = np.mean(y[t-k: t+k])
+            for t in range(1, len(y)-1):
+                y_hat[t, 0] = np.mean(y[t-1: t+1])
         else:
-            for t in range(k-1, len(y)):
-                y_hat[t, 0] = np.mean(y[t-(k-1): t+k+1])
+            for t in range(k, len(y)-rem):
+                y_hat[t, 0] = np.mean(y[t-(k-1): t+(k+1)+1])
 
         if sf:
             if m == 2:
-                return y_hat[k:, 0]
+                return y_hat[1:(len(y)-1), 0], 1, len(y)-1
             else:
-                return y_hat[(k-1):, 0]
+                return y_hat[k:(len(y)-rem), 0], k, len(y)-rem
         else:
             sf_ma_order = sm
             if sm is None:
@@ -56,13 +57,15 @@ def ma(y, m=None, sm=None, sf=False):
                     sf_ma_order = int(input('Enter the order of the second fold:\n'))
                     if sf_ma_order > 0:
                         break
-            tmp_so_ma = ma(y_hat[(k-1):, 0], m=sf_ma_order, sf=True)
-            y_hat[-len(tmp_so_ma):, 1] = tmp_so_ma
-            return y_hat[:, 1]
+            if m == 2:
+                return ma(y_hat[1:(len(y) - 1), 0], m=sf_ma_order, sf=True)
+            else:
+                return ma(y_hat[k:(len(y) - rem), 0], m=sf_ma_order, sf=True)
 
 y = data.Temp.values
-yhat = ma(y)
-
+yhat, start, end = ma(y)
+# sample = [18872.2, 1906.1, 2019.8, 2180.3, 2388.1, 2449.0, 2180.3, 2388.1, 2449.0]
+# tmp_yhat, start, end = ma(sample)
 #%%
 
 
@@ -74,10 +77,12 @@ r_idx = 0
 c_idx = 0
 max_samples = 50
 for i in range(len(ma_orders)):
-    yhat = ma(y=y, m=ma_orders[i])
+    yhat, start, end = ma(y=y, m=ma_orders[i])
     k = ma_orders[i]//2 + 1
-    axes[r_idx, c_idx].plot(y[(k-1):((k-1)+max_samples)], color='blue', label='Original')
-    axes[r_idx, c_idx].plot(yhat[:max_samples], color='orange', label='Smoothed')
+    axes[r_idx, c_idx].plot(data.Date.iloc[:max_samples],
+                            y[:max_samples], color='blue', label='Original')
+    axes[r_idx, c_idx].plot(data.Date.iloc[start:start+max_samples],
+                            yhat[:max_samples], color='orange', label='Smoothed')
     axes[r_idx, c_idx].set_ylabel('Smoothed value')
     axes[r_idx, c_idx].set_xlabel('Time t')
     axes[r_idx, c_idx].set_title(f'MA({ma_orders[i]})')
@@ -99,10 +104,11 @@ max_samples = 50
 for i in range(len(ma_orders)):
     ma_f_order = ma_orders[i][1]
     ma_s_order = ma_orders[i][0]
-    yhat = ma(y=y, m=ma_f_order, sm=ma_s_order)
-    k = ma_f_order//2 + 1
-    axes[r_idx, c_idx].plot(y[(k - 1):((k - 1) + max_samples)], color='blue', label='Original')
-    axes[r_idx, c_idx].plot(yhat[(k-1):((k-1)+max_samples)], color='orange', label='Smoothed')
+    yhat, start, end = ma(y=y, m=ma_f_order, sm=ma_s_order)
+    axes[r_idx, c_idx].plot(data.Date.iloc[:max_samples],
+                            y[:max_samples], color='blue', label='Original')
+    axes[r_idx, c_idx].plot(data.Date.iloc[start:start+max_samples],
+                            yhat[:max_samples], color='orange', label='Smoothed')
     axes[r_idx, c_idx].set_ylabel('Smoothed value')
     axes[r_idx, c_idx].set_xlabel('Time t')
     axes[r_idx, c_idx].set_title(f'MA({ma_orders[i]})')
