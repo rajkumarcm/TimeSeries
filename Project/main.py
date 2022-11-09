06e99537eb72cadd2e9d053396a828c4deeb0ad2
Rajkumar Conjeevaram Mohan <rajkumarcm@yahoo.com>
2022-11-09 14:18:39 -0500

    Merging Arizona weather with Pollutant dataframe

--- Project/main.py
+++ Project/main.py
@@ -78,21 +78,21 @@ print(f'List of dates at which values are missing:\n'
       f'{missing_dates}')
 
 #%% Let's plot, and visualize the data until the point in time the values are incomplete
-subset_nmissing = uspoll_state.loc[uspoll_state['Date Local'] < missing_dates.min(), ['Date Local', 'CO AQI']]
+subset_nmissing_co_aqi = uspoll_state.loc[uspoll_state['Date Local'] < missing_dates.min(), ['Date Local', 'CO AQI']]
 plt.figure()
-subset_nmissing['CO AQI'].plot()
+subset_nmissing_co_aqi['CO AQI'].plot()
 plt.show()
 
 
 #%%
-subset_nmissing = subset_nmissing.set_index('Date Local').asfreq('D')
+subset_nmissing_co_aqi = subset_nmissing_co_aqi.set_index('Date Local').asfreq('D')
 
 #%%
 mse_list = [np.Inf, np.Inf]
 for i in range(2, 380):
-    ets = ETS.ExponentialSmoothing(subset_nmissing, trend=None, damped_trend=False,
+    ets = ETS.ExponentialSmoothing(subset_nmissing_co_aqi, trend=None, damped_trend=False,
                                    seasonal="mul", seasonal_periods=i).fit()
-    y_true = subset_nmissing.reset_index(drop=True).values.reshape([-1])
+    y_true = subset_nmissing_co_aqi.reset_index(drop=True).values.reshape([-1])
     y_pred = ets.fittedvalues.reset_index(drop=True).values.reshape([-1])
     diff = y_true - y_pred
     mse = 1/len(diff) * (diff.T @ diff)
@@ -101,17 +101,94 @@ for i in range(2, 380):
 #%%
 seasonality_period = np.argmin(mse_list) + 1 # + 1 due to indices starting from 0
 
-ets = ETS.ExponentialSmoothing(subset_nmissing, trend=None, damped_trend=False,
-          seasonal="mul", seasonal_periods=375).fit()
-imputed_data = ets.forecast(steps=missing_dates.shape[0])
+ets_co_aqi = ETS.ExponentialSmoothing(subset_nmissing_co_aqi, trend=None, damped_trend=False,
+          seasonal="mul", seasonal_periods=seasonality_period).fit()
+imputed_data = ets_co_aqi.forecast(steps=missing_dates.shape[0])
 
 plt.figure()
-plt.plot(subset_nmissing.reset_index(drop=True), '-b')
-plt.plot(ets.fittedvalues.reset_index(drop=True), '-r')
-# plt.xlim([0, 30])
+plt.plot(subset_nmissing_co_aqi.reset_index(drop=True), '-b')
+plt.plot(ets_co_aqi.fittedvalues.reset_index(drop=True), '-r')
 plt.show()
 
+#%% Impute the NO AQI
+subset_nmissing_so2_aqi = uspoll_state.loc[uspoll_state['Date Local'] < missing_dates.min(), ['Date Local', 'SO2 AQI']]
+
 #%%
+plt.figure()
+subset_nmissing_so2_aqi['SO2 AQI'].plot()
+# plt.xlim(['2000-01-01', '2000-04-01'])
+plt.show()
+
+#%% O3 subset and plot
+subset_nmissing_o3_aqi = uspoll_state.loc[uspoll_state['Date Local'] < missing_dates.min(), ['Date Local', 'O3 AQI']]
+subset_nmissing_o3_aqi = subset_nmissing_o3_aqi.set_index('Date Local').asfreq('D')
+plt.figure()
+subset_nmissing_o3_aqi['O3 AQI'].plot()
+plt.show()
+
+#%% Holts Winter Optimization Loop
+
+mse_list_o3_aqi = [np.Inf, np.Inf]
+for i in range(2, 380):
+    ets_o3 = ETS.ExponentialSmoothing(subset_nmissing_o3_aqi, trend=None, damped_trend=False,
+                                      seasonal="mul", seasonal_periods=i).fit()
+    y_true = subset_nmissing_o3_aqi.reset_index(drop=True).values.reshape([-1])
+    y_pred = ets_o3.fittedvalues.reset_index(drop=True).values.reshape([-1])
+    diff = y_true - y_pred
+    mse = 1/len(diff) * (diff.T @ diff)
+    mse_list_o3_aqi.append(mse)
+
+
+#%% O3 Holts Winter Seasonal Fit
+seasonality_period_o3_aqi = np.argmin(mse_list_o3_aqi) + 1 # + 1 due to indices starting from 0
+
+ets_o3_aqi = ETS.ExponentialSmoothing(subset_nmissing_o3_aqi, trend=None, damped_trend=False,
+               seasonal="mul", seasonal_periods=seasonality_period_o3_aqi).fit()
+imputed_data_o3_aqi = ets_o3_aqi.forecast(steps=missing_dates.shape[0])
+
+plt.figure()
+plt.plot(subset_nmissing_o3_aqi, '-b')
+plt.plot(ets_o3_aqi.fittedvalues, '-r')
+plt.title('O3 Air Quality Index')
+plt.ylabel('Air Quality Index')
+plt.show()
+
+#%% O3 AQI Holt Winter Seasonal Imputation
+second_part_nmissing_o3_aqi = uspoll_state.loc[uspoll_state['Date Local'] > missing_dates.max(), ['Date Local', 'O3 AQI']]
+second_part_nmissing_o3_aqi = second_part_nmissing_o3_aqi.reset_index(drop=True).set_index('Date Local')
+imputed_data_o3_aqi_mod = pd.DataFrame({'Date Local':missing_dates.reset_index(drop=True),
+                                        'O3 AQI':imputed_data_o3_aqi.reset_index()[0]})
+imputed_data_o3_aqi_mod = imputed_data_o3_aqi_mod.set_index('Date Local')
+collated_o3_aqi = pd.concat([subset_nmissing_o3_aqi,
+                             imputed_data_o3_aqi_mod, second_part_nmissing_o3_aqi], axis=0)
+
+
+
+#%% Merge two dataframes
+weather_df = pd.read_csv('arizona_weather.csv', header=0)
+weather_df['Date Local'] = pd.to_datetime(weather_df['Date Local'])
+joined_df = pd.merge(left=weather_df, right=collated_o3_aqi.reset_index(), how='inner',
+                     on='Date Local', sort=False)
+
+#%%
+
+
+#%% Load independent variables
+ind_cnames = ['Year', 'Day_of_Year', 'Station_Number', 'Air_Temp_Max', 'Air_Temp_Min', 'Air_Temp_Mean',
+              'Rel_Humdity_Max', 'Rel_Humidity_Min', 'Rel_Humidity_Mean', 'Vapor_Pressure_Deficit_Mean',
+              'Solar_Radiation_Total', 'Precipitation_Total', '4_inch_soil_Max', '4_inch_soil_Min',
+              '4_inch_soil_Mean', '20_inch_soil_Max', '20_inch_soil_Min', '20_inch_soil_Mean',
+              'wind_speed_Mean',
+              'wind_vector_mag', 'wind_vector_dir', 'wind_dir_std', 'max_wind_speed', 'heat_units',
+              'reference']
+for i in range(2000, 2016+1):
+    tmp = pd.read_csv(f'Arizona\\{i}.txt', header=None)
+
+
+
+
+
+
 
 
 
---
+++ Project/preprocess_climate.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+import matplotlib
+from matplotlib import pyplot as plt
+
+main_df = None
+
+ind_cnames = ['Year', 'Day_of_Year', 'Station_Number', 'Air_Temp_Max', 'Air_Temp_Min', 'Air_Temp_Mean',
+              'Rel_Humdity_Max', 'Rel_Humidity_Min', 'Rel_Humidity_Mean', 'Vapor_Pressure_Deficit_Mean',
+              'Solar_Radiation_Total', 'Precipitation_Total', '4_inch_soil_Max', '4_inch_soil_Min',
+              '4_inch_soil_Mean', '20_inch_soil_Max', '20_inch_soil_Min', '20_inch_soil_Mean',
+              'wind_speed_Mean',
+              'wind_vector_mag', 'wind_vector_dir', 'wind_dir_std', 'max_wind_speed', 'heat_units',
+              'reference']
+for i in range(2000, 2016+1):
+    # if i == 2003:
+    #     print('breakpoint...')
+    tmp = pd.read_csv(f'Arizona\\{i}.txt', header=None,
+                      skip_blank_lines=True)
+    tmp = tmp.iloc[:, :len(ind_cnames)].set_axis(ind_cnames, axis=1)
+
+    dates_df = pd.date_range(start=f'{i}-01-01', end=f'{i}-12-31', freq='D')
+    dates_df = pd.DataFrame({'Date Local':dates_df, 'Day_of_Year':dates_df.dayofyear})
+    tmp = tmp.iloc[:dates_df.shape[0]] # upto max_wind_speed
+    tmp['Day_of_Year'] = tmp['Day_of_Year'].astype(int)
+    # tmp = tmp.sort_values(by='Day_of_Year', ascending=True, ignore_index=True)
+    tmp = pd.merge(left=dates_df, right=tmp, how='left', on='Day_of_Year', sort=False)
+
+    if main_df is None:
+        main_df = tmp
+    else:
+        main_df = pd.concat([main_df, tmp], axis=0)
+
+
+
+print('debug...')
