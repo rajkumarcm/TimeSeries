c976a97a3e3b79b3c0ad52ed5c9cc33f0ae1a669
Rajkumar Conjeevaram Mohan <rajkumarcm@yahoo.com>
2022-11-12 18:30:41 -0500

    Upto stationarizing signal

--- Project/main.py
+++ Project/main.py
@@ -8,6 +8,7 @@ from os.path import abspath, join
 from statsmodels.tsa import holtwinters as ETS
 from Utilities.WhitenessTest import WhitenessTest as WT
 from Utilities.Correlation import Correlation as Corr
+from statsmodels.tsa.seasonal import STL
 
 #%% Load the data
 uspoll = pd.read_csv('pollution_us_2000_2016.csv', header=0, index_col=0)
@@ -29,10 +30,11 @@ axes[0].set_ylabel('Start Date')
 axes[0].set_title('Start date in each state')
 
 axes[1].stem(last_date_by_state.index.values, last_date_by_state['Date Local'])
-# axes[1].axhline(y=last_date_by_state.max(), color='r')
+axes[1].axhline(y=last_date_by_state.mode(), color='r')
 axes[1].tick_params(labelrotation=90)
 axes[1].set_ylabel('Last Date')
 axes[1].set_title('Last date in each state')
+fig.tight_layout()
 plt.show()
 
 #%% Replace NaN's in CO AQI
@@ -143,7 +145,7 @@ for i in range(2, 380):
 seasonality_period_o3_aqi = np.argmin(mse_list_o3_aqi) + 1 # + 1 due to indices starting from 0
 
 ets_o3_aqi = ETS.ExponentialSmoothing(subset_nmissing_o3_aqi, trend=None, damped_trend=False,
-               seasonal="mul", seasonal_periods=seasonality_period_o3_aqi).fit()
+               seasonal="additive", seasonal_periods=seasonality_period_o3_aqi).fit()
 imputed_data_o3_aqi = ets_o3_aqi.forecast(steps=missing_dates.shape[0])
 
 plt.figure()
@@ -170,31 +172,83 @@ weather_df['Date Local'] = pd.to_datetime(weather_df['Date Local'])
 joined_df = pd.merge(left=weather_df, right=collated_o3_aqi.reset_index(), how='inner',
                      on='Date Local', sort=False)
 
+#%% Correlation plot
+
+fig = plt.figure(figsize=(15, 15))
+sns.heatmap(joined_df.corr(method='pearson'), linewidths=0.8)
+plt.tick_params(axis='both', labelsize=20)
+plt.title('Correlation Plot', fontsize=20)
+fig.tight_layout()
+plt.show()
+
 #%%
+plt.figure(figsize=(12, 8))
+plt.plot(subset_nmissing_o3_aqi, '-b', label='Original Data')
+plt.plot(imputed_data_o3_aqi_mod, '-r', label='Imputed Data')
+plt.plot(second_part_nmissing_o3_aqi, '-b', label='Original Data')
+plt.ylabel('AQI Value')
+plt.title('Arizona Air Quality Index from 2000 - March 2016')
+plt.show()
 
 
-#%% Load independent variables
-ind_cnames = ['Year', 'Day_of_Year', 'Station_Number', 'Air_Temp_Max', 'Air_Temp_Min', 'Air_Temp_Mean',
-              'Rel_Humdity_Max', 'Rel_Humidity_Min', 'Rel_Humidity_Mean', 'Vapor_Pressure_Deficit_Mean',
-              'Solar_Radiation_Total', 'Precipitation_Total', '4_inch_soil_Max', '4_inch_soil_Min',
-              '4_inch_soil_Mean', '20_inch_soil_Max', '20_inch_soil_Min', '20_inch_soil_Mean',
-              'wind_speed_Mean',
-              'wind_vector_mag', 'wind_vector_dir', 'wind_dir_std', 'max_wind_speed', 'heat_units',
-              'reference']
-for i in range(2000, 2016+1):
-    tmp = pd.read_csv(f'Arizona\\{i}.txt', header=None)
+#%%
+wt = WT(collated_o3_aqi)
+wt.Plot_Rolling_Mean_Var(name='O3 Air Quality Index')
+
+#%% De-Seasonalize the data
+stl = STL(collated_o3_aqi)
+res = stl.fit()
+T = res.trend
+S = res.seasonal
+R = res.resid
+res.plot()
+Var_R = R.var()
+Var_T = T.var()
+Var_S = S.var()
+strength_of_trend = np.max([0, 1-(Var_R/(T+R).var())])
+strength_of_seasonality = np.max([0, 1-(Var_R/(S+R).var())])
+print(f'Strength of the trend {100 * strength_of_trend:.2f}')
+print(f'Strength of the seasonality {100 * strength_of_seasonality:.2f}')
+
+#%%
+deseasoned_o3_aqi = collated_o3_aqi - S.reset_index().set_axis(axis=1, labels=['Date Local', 'O3 AQI']).set_index('Date Local')
+plt.figure()
+deseasoned_o3_aqi.plot()
+plt.show()
+
+#%%
+deseasoned_o3_aqi = np.log(collated_o3_aqi)
+deseasoned_o3_aqi = deseasoned_o3_aqi.diff()
 
 
+#%%
+wt = WT(deseasoned_o3_aqi['O3 AQI'].iloc[1:])
+fig, axes = plt.subplots(3, 1, figsize=(9, 15))
+deseasoned_o3_aqi.plot(ax=axes[0])
+axes[0].set_ylabel('O3 AQI Value')
+axes[0].set_title('Stationary O3 AQI data')
 
+wt.Plot_Rolling_Mean_Var(name='Stationary O3 AQI', axes=axes, start_idx=1)
+plt.show()
 
+#%%
+corr = Corr()
+corr.acf(x=deseasoned_o3_aqi['O3 AQI'].iloc[1:], max_lag=100)
 
+#%% Second order differencing
+deseasoned_o3_aqi_2nd_order = deseasoned_o3_aqi.diff().iloc[2:]
+corr.acf(x=deseasoned_o3_aqi_2nd_order['O3 AQI'].iloc[1:], max_lag=100)
 
+#%%
+fig, axes = plt.subplots(1, 2, figsize=(15, 6))
+weather_df[['Rel_Humdity_Max', 'Rel_Humidity_Min', 'Rel_Humidity_Mean']].plot.box(showfliers=False, ax=axes[0])
+axes[0].set_title('Relative Humidity')
+axes[0].set_ylabel('Rel Humidity Value')
 
-
-
-
-
-
+weather_df[['Air_Temp_Max', 'Air_Temp_Min', 'Air_Temp_Mean']].plot.box(showfliers=False, ax=axes[1])
+axes[1].set_title('Air Temperature')
+axes[1].set_ylabel('Degree Centigrade')
+plt.show()
 
 
 
