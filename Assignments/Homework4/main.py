61c173c3cc23d8424d61f67f85e6ad8bf4845c92
Rajkumar Conjeevaram Mohan <rajkumarcm@yahoo.com>
2022-11-24 09:13:59 -0500

    GPAC: Need to correct negative values

---
+++ Assignments/Homework4/main.py
@@ -0,0 +1,113 @@
+#%%
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from statsmodels import api as sm
+from statsmodels.tsa.arima_process import ArmaProcess
+from Utilities.Correlation import Correlation as Corr
+import re
+np.random.seed(123)
+
+#%%
+ar = [1, 0.5]
+ma = [1]
+arma_process = ArmaProcess(ar, ma)
+
+#%% Q1.
+# def arma_generate_data():
+#     T = int(input('Enter the number of data samples'))
+#     mu = float(input('Enter the mean of white noise'))
+#     var = float(input('Enter the variance of the white noise'))
+#     std = np.sqrt(var)
+#     ar_coeffs = None
+#     ma_coeffs = None
+#     pattern = "[0-9]+[\.0-9]*"
+#     while True:
+#         na = int(input('Enter the AR order'))
+#         coeffs_txt = input('Enter the coefficients of AR - Separate them by commas')
+#         ar_coeffs = re.findall(pattern, coeffs_txt)
+#         if(len(ar_coeffs)==na+1):
+#             break
+#
+#     while True:
+#         nb = int(input('Enter the MA order'))
+#         coeffs_txt = input('Enter the coefficients of MA - Separate them by commas')
+#         ma_coeffs = re.findall(pattern, coeffs_txt)
+#         if(len(ma_coeffs)==nb+1):
+#             break
+#
+#     arma_process = ArmaProcess(ma_coeffs, ar_coeffs)
+#     return arma_process.generate_sample(T, scale=std) + mu
+#
+# data = arma_generate_data()
+# print('breakpoint...')
+
+#%%
+# Q2. GPAC table
+
+def gpac_table(x, na, nb):
+
+    def gpac(acf_vals, j, k):
+        num = np.zeros([k, k])
+        for p in range((k - 1)):  # for all columns except the last one
+            offset = j - p
+            tmp_col = []
+            for q in range(offset, offset + k):
+                # tmp_col.append(np.abs(q)) # FOR DEBUGGING PURPOSES
+                tmp_col.append(acf_vals[np.abs(q)])
+            num[:, p] = tmp_col
+
+        den = np.copy(num)
+        den[:, k-1] = acf_vals[np.abs(list(range(j-k+1, j+1)))]
+        tmp_col = []
+        for q in range(j+1, j+k+1):
+            tmp_col.append(acf_vals[np.abs(q)])
+        num[:, k-1] = tmp_col
+        return np.linalg.det(num)/np.linalg.det(den)
+
+    corr = Corr()
+    acf_vals, _ = corr.acf(x=x, max_lag=na+nb, plot=False, return_acf=True)
+    Q = np.zeros([nb+1, na+1])
+    for j in range(nb+1):
+        for k in range(1, na+1):
+            if k == 1:
+                Q[j, k] = acf_vals[j+1]/acf_vals[j]
+            else:
+                # gpac(acf_vals, 1, 2) # FOR DEBUGGING PURPOSE...
+                Q[j, k] = gpac(acf_vals, j, k)
+    return Q[:, 1:] # because k=0 does not exist
+
+def arma_gpac():
+    T = int(input('Enter the number of data samples'))
+    mu = float(input('Enter the mean of white noise'))
+    var = float(input('Enter the variance of the white noise'))
+    std = np.sqrt(var)
+    ar_coeffs = None
+    ma_coeffs = None
+    pattern = "[\-]*[0-9]+[\.0-9]*"
+    while True:
+        na = int(input('Enter the AR order'))
+        coeffs_txt = input('Enter the coefficients of AR - Separate them by commas')
+        ar_coeffs = re.findall(pattern, coeffs_txt)
+        ar_coeffs = list(map(lambda x: float(x), ar_coeffs))
+        if(len(ar_coeffs)==na+1):
+            break
+
+    while True:
+        nb = int(input('Enter the MA order'))
+        coeffs_txt = input('Enter the coefficients of MA - Separate them by commas')
+        ma_coeffs = re.findall(pattern, coeffs_txt)
+        ma_coeffs = list(map(lambda x: float(x), ma_coeffs))
+        if(len(ma_coeffs)==nb+1):
+            break
+
+    arma_process = ArmaProcess(ar_coeffs, ma_coeffs)
+    x = arma_process.generate_sample(T, scale=std) + mu
+    return gpac_table(x, na+2, nb+2)
+
+data = arma_gpac()
+print('breakpoint...')
+
+
+
+
